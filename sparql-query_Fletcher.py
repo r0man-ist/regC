3b98bbe53d5c29624735c82170bb078386bdf3f2
RK <roman.k@gmx.com>
2024-01-22 20:11:26 +0000

    initial commit query

---
+++ sparql-query_Fletcher.py
@@ -0,0 +1,29 @@
+# SPARQL Query to identify books bought from Stephen Fletcher
+
+from rdflib import Graph
+
+
+
+g = Graph()
+with open("RegC.rdf", "r") as f:
+    result = g.parse(f, format="application/rdf+xml")
+
+query = """
+PREFIX dcterms: <http://purl.org/dc/terms>
+SELECT ?Item ?ItemTitle ?ItemShelfmark
+WHERE {
+    ?s crm:P23_transferred_title_from ?Seller .
+    ?s rdf:type crm:E96_Purchase .
+    ?Seller rdfs:label ?SellerLabel .
+    ?s crm:P24_transferred_title_of ?Item .
+    ?Item dcterms:title ?ItemTitle .
+    OPTIONAL { ?Item crm:P48_has_preferred_identifier ?ItemShelfmark }
+    FILTER (?Seller = <https://data.cerl.org/thesaurus/cnp00058554>) .
+    }"""
+
+qres = g.query(query)
+
+print(len(qres))
+for row in qres:
+    print(f"{row.ItemTitle} || {row.ItemShelfmark}")
+
